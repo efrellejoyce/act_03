d1b4f3ce74a14ad7a1c1c8eea82c6b45101f1bd7
Efrelle Joyce Ducyao <efrellejoyced@gmail.com>
2024-10-22 08:20:51 +0700

    initial commit

---
+++ arrayObjectActivity.js
@@ -0,0 +1,13 @@
+let car = {
+    type: "Hyundai",
+    model: "Acceent",
+    color: "Black"
+};
+
+console.log(typeof car);
+
+car.type = "Hyundai";
+console.log(car);
+
+car.wheels = 4;
+console.log(car);
